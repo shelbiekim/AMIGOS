5f786be13bd53e992ff84636ddd571ec6f92fa3d
Miji Kim <mkim0021@student.monash.edu>
2020-10-26 07:16:45 +1100

    Add Ssend Email feature

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Amigos.Models;
+using Amigos.Utils;
 
 namespace Amigos.Controllers
 {
@@ -27,5 +29,40 @@ namespace Amigos.Controllers
 
             return View();
         }
+
+        public ActionResult Send_Email()
+        {
+            return View(new SendEmailViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Send_Email(SendEmailViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    String toEmail = model.ToEmail;
+                    String subject = model.Subject;
+                    String contents = model.Contents;
+
+                    EmailSender es = new EmailSender();
+                    es.Send(toEmail, subject, contents);
+
+                    ViewBag.Result = "Email has been sent.";
+
+                    ModelState.Clear();
+
+                    return View(new SendEmailViewModel());
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+
+            return View();
+        }
+
     }
 }
---
+++ Utils/EmailSender.cs
@@ -0,0 +1,27 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Amigos.Utils
+{
+    public class EmailSender
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "SG.OZ7nrv4WRDan9DD7hAs7OQ.mvouMhOnKb29g3QA6579md9bO73yJt5rArwwxWAD9HY";
+
+        public void Send(String toEmail, String subject, String contents)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("kimshelbie@gmail.com", "Amigos test");
+            var to = new EmailAddress("shelbkim@gmail.com", "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = client.SendEmailAsync(msg);
+        }
+    }
+}
