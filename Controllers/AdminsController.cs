4cd43885ee39255c2e570d0b925fb5c7b1d1e41c
Miji Kim <mkim0021@student.monash.edu>
2020-11-04 01:03:26 +1100

    Update Send_Email layout

--- Controllers/AdminsController.cs
+++ Controllers/AdminsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Amigos.Models;
+using Amigos.Utils;
 
 namespace Amigos.Controllers
 {
@@ -19,8 +21,64 @@ namespace Amigos.Controllers
             // redirect logged-in users who are not administrators
             else
             {
+              
                 return Redirect("~/Home");
             }
+            
+        }
+
+        [Authorize]
+        public ActionResult Send_Email()
+        {
+            if (User.IsInRole("Administrator"))
+            {
+                return View(new SendEmailViewModel());
+            }
+            // redirect logged-in users who are not administrators
+            else
+            {
+
+                return Redirect("~/Home");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Send_Email(SendEmailViewModel model)
+        {
+            if (User.IsInRole("Administrator"))
+            {
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        String toEmail = model.ToEmail;
+                        String subject = model.Subject;
+                        String contents = model.Contents;
+
+                        EmailSender es = new EmailSender();
+                        es.Send(toEmail, subject, contents);
+
+                        ViewBag.Result = "Email has been sent.";
+
+                        ModelState.Clear();
+
+                        return View(new SendEmailViewModel());
+                    }
+                    catch
+                    {
+                        return View();
+                    }
+                }
+
+                return View();
+            }
+            // redirect logged-in users who are not administrators
+            else
+            {
+
+                return Redirect("~/Home");
+            }
+            
         }
     }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -38,39 +38,5 @@ namespace Amigos.Controllers
             return View();
         }
 
-        public ActionResult Send_Email()
-        {
-            return View(new SendEmailViewModel());
-        }
-
-        [HttpPost]
-        public ActionResult Send_Email(SendEmailViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    String toEmail = model.ToEmail;
-                    String subject = model.Subject;
-                    String contents = model.Contents;
-
-                    EmailSender es = new EmailSender();
-                    es.Send(toEmail, subject, contents);
-
-                    ViewBag.Result = "Email has been sent.";
-
-                    ModelState.Clear();
-
-                    return View(new SendEmailViewModel());
-                }
-                catch
-                {
-                    return View();
-                }
-            }
-
-            return View();
-        }
-
     }
 }
